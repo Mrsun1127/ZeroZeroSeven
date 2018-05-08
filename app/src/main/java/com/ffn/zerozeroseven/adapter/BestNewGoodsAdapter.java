e05defc3229dee668fb1a033880ef95b8ab49d81
mrsun <mrsun1127@163.com>
2018-05-08 10:19:46 +0800

    fristCommit

--- app/src/main/java/com/ffn/zerozeroseven/adapter/BestNewGoodsAdapter.java
+++ app/src/main/java/com/ffn/zerozeroseven/adapter/BestNewGoodsAdapter.java
@@ -5,6 +5,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
@@ -28,24 +29,45 @@ public class BestNewGoodsAdapter extends BaseRecyclerAdapter<BestNewShowInfo.Dat
     }
 
     @Override
-    protected void onBindDefaultViewHolder(RecyclerView.ViewHolder holder, BestNewShowInfo.DataBean.LatestGoodsBean info, int position) {
+    protected void onBindDefaultViewHolder(RecyclerView.ViewHolder holder, BestNewShowInfo.DataBean.LatestGoodsBean info,final int position) {
         MViewHolder mHolder = (MViewHolder) holder;
         Glide.with(mContext)
                 .load(info.getThumbnail())
                 .error(R.drawable.oops)
                 .into(mHolder.imageView);
         mHolder.tv_name.setText(info.getGoodsName());
+        mHolder.rl_add.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                if (imageClick != null) {
+                    imageClick.onClick(v, position);
+                }
+            }
+        });
     }
 
     private class MViewHolder extends RecyclerView.ViewHolder {
         ImageView imageView;
         TextView tv_name;
+        RelativeLayout rl_add;
         MViewHolder(View itemView) {
             super(itemView);
-
             imageView=itemView.findViewById(R.id.iv_icon);
             tv_name=itemView.findViewById(R.id.tv_name);
+            rl_add=itemView.findViewById(R.id.rl_add);
 
         }
+    }
+
+
+    private OnItemImageClick imageClick;
+
+    public void setOnItemImageViewClick(OnItemImageClick imageClick) {
+        this.imageClick = imageClick;
+    }
+
+    public interface OnItemImageClick {
+        void onClick(View view, int position);
     }
 }
