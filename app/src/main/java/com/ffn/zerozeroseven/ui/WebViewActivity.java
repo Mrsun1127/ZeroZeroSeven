1b744fa33bf5de64c2d79af47ec6f436d7c6d54c
mrsun <mrsun1127@163.com>
2018-05-22 11:40:46 +0800

    fristCommit

--- app/src/main/java/com/ffn/zerozeroseven/ui/WebViewActivity.java
+++ app/src/main/java/com/ffn/zerozeroseven/ui/WebViewActivity.java
@@ -6,14 +6,14 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+import com.alibaba.fastjson.JSON;
 import com.ffn.zerozeroseven.R;
 import com.ffn.zerozeroseven.base.BaseActivity;
+import com.ffn.zerozeroseven.base.BaseAppApplication;
 import com.ffn.zerozeroseven.bean.WebInfo;
-import com.ffn.zerozeroseven.bean.requsetbean.WebUserInfo;
-import com.ffn.zerozeroseven.utlis.LogUtils;
+import com.ffn.zerozeroseven.view.TopView;
 
-import java.util.HashMap;
-import java.util.Map;
+
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -21,6 +21,8 @@ import butterknife.ButterKnife;
 public class WebViewActivity extends BaseActivity {
     @Bind(R.id.webView)
     WebView webView;
+    @Bind(R.id.topView)
+    TopView topView;
 
     @Override
     protected int setLayout() {
@@ -31,7 +33,7 @@ public class WebViewActivity extends BaseActivity {
     public void initView() {
         super.initView();
         ButterKnife.bind(this);
-        webView.addJavascriptInterface(new JSHook(),"hello");
+        webView.addJavascriptInterface(new JSHook(), "hello");
         WebSettings settings = webView.getSettings();
         settings.setJavaScriptEnabled(true);
         settings.setSupportZoom(true);
@@ -40,21 +42,66 @@ public class WebViewActivity extends BaseActivity {
         webView.setWebChromeClient(new WebChromeClient());
         webView.setWebViewClient(new WebViewClient());
         webView.loadUrl(getIntent().getStringExtra("url"));
+        topView.setOnTitleListener(new TopView.OnTitleClickListener() {
+            @Override
+            public void Right() {
+
+            }
+
+            @Override
+            public void Back() {
+                if (webView.canGoBack()) {
+                    webView.goBack();
+                } else {
+                    finish();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            finish();
+        }
     }
 
     @Override
     protected void doMain() {
 
     }
-    public class JSHook{
+
+    public class JSHook {
         @JavascriptInterface
-        public Map<String, String> requestDate(){
-            Map<String,String> map=new HashMap<>();
-            map.put("id",schoolIId);
-            map.put("phone",userInfo.getPhone());
-            map.put("token",userInfo.getToken());
-            LogUtils.D("tol",schoolIId+userInfo.getPhone()+userInfo.getToken());
-            return  map;
+        public String requestDate() {
+            WebInfo webInfo = new WebInfo();
+            webInfo.setId(schoolIId);
+            webInfo.setToken(userInfo.getToken());
+            webInfo.setPhone(userInfo.getPhone());
+            return JSON.toJSONString(webInfo);
         }
+
+        @JavascriptInterface
+        public void payMoney() {
+            BaseAppApplication.mainHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    HomeActivity.getmInstance().get().go2Fragment(1);
+                }
+            }, 500);
+            finish();
+
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        webView.setWebChromeClient(null);
+        webView.setWebViewClient(null);
+        webView.getSettings().setJavaScriptEnabled(false);
+        webView.clearCache(true);
     }
 }
