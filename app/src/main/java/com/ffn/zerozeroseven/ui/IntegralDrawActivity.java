0b7f6c6278e027712cf49bfbfbaeecdddd117300
mrsun <mrsun1127@163.com>
2018-05-29 17:06:17 +0800

    fristCommit

--- app/src/main/java/com/ffn/zerozeroseven/ui/IntegralDrawActivity.java
+++ app/src/main/java/com/ffn/zerozeroseven/ui/IntegralDrawActivity.java
@@ -1,19 +1,19 @@
 package com.ffn.zerozeroseven.ui;
 
-import android.support.v7.widget.GridLayoutManager;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.widget.RecyclerView;
-import android.view.MotionEvent;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.ProgressBar;
 
 import com.ffn.zerozeroseven.R;
 import com.ffn.zerozeroseven.adapter.InteralAdapter;
-import com.ffn.zerozeroseven.base.BaseActivity;
+import com.ffn.zerozeroseven.base.BaseFullActivity;
+import com.ffn.zerozeroseven.utlis.ToastUtils;
 import com.ffn.zerozeroseven.view.FullyGridLayoutManager;
 import com.ffn.zerozeroseven.view.GridSpacingItemDecoration;
-import com.ffn.zerozeroseven.view.TopView;
 import com.ffn.zerozeroseven.view.mainscroll.SmoothRefreshLayout;
-import com.scwang.smartrefresh.layout.SmartRefreshLayout;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,15 +21,16 @@ import java.util.List;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class IntegralDrawActivity extends BaseActivity {
+public class IntegralDrawActivity extends BaseFullActivity {
     @Bind(R.id.refreshlayout)
     SmoothRefreshLayout refreshlayout;
-    @Bind(R.id.topView)
-    TopView topView;
-    @Bind(R.id.recycleview)
+    @Bind(R.id.recyclerView_with_recyclerView_in_coordinatorLayout)
     RecyclerView recycleview;
-
     private InteralAdapter adapter;
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
+    @Bind(R.id.colllayout)
+    CollapsingToolbarLayout colllayout;
 
     @Override
     protected int setLayout() {
@@ -39,18 +40,29 @@ public class IntegralDrawActivity extends BaseActivity {
     @Override
     public void initView() {
         ButterKnife.bind(this);
-        topView.setTopText("积分抽奖");
-        topView.setOnTitleListener(new TopView.OnTitleClickListener() {
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(R.drawable.bitis_arrow);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
-            public void Right() {
-
-            }
-
-            @Override
-            public void Back() {
-                finish();
+            public void onClick(View v) {
+                onBackPressed();
             }
         });
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.more:
+                        break;
+                    case R.id.minesigh:
+                        break;
+                    case R.id.qiandao:
+                        break;
+                }
+                return true;    //返回为true
+            }
+        });
+        getSupportActionBar().setTitle("");
         recycleview.setLayoutManager(new FullyGridLayoutManager(this, 2));
         recycleview.addItemDecoration(new GridSpacingItemDecoration(2, 10, false));
         adapter = new InteralAdapter(this);
@@ -58,8 +70,14 @@ public class IntegralDrawActivity extends BaseActivity {
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar, menu); //解析menu布局文件到menu
+        return true;
+    }
+
+    @Override
     protected void doMain() {
-        List<String> s=new ArrayList<>();
+        List<String> s = new ArrayList<>();
         s.add("1");
         s.add("1");
         s.add("1");
