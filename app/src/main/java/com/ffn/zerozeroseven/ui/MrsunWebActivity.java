110812f1efce99592a9781c9c0d46268048aed15
mrsun <mrsun1127@163.com>
2018-06-03 15:59:55 +0800

    fristCommit

--- app/src/main/java/com/ffn/zerozeroseven/ui/MrsunWebActivity.java
+++ app/src/main/java/com/ffn/zerozeroseven/ui/MrsunWebActivity.java
@@ -19,6 +19,7 @@ public class MrsunWebActivity extends BaseActivity {
     @Bind(R.id.ll_web)
     LinearLayout ll_web;
     AgentWeb mAgentWeb;
+
     @Override
     protected int setLayout() {
         return R.layout.activity_web;
@@ -45,21 +46,25 @@ public class MrsunWebActivity extends BaseActivity {
 
             }
         });
-        String title=getIntent().getStringExtra("title");
-        if(!TextUtils.isEmpty(title)){
-            titleView.setTopText(title);
+        String title = getIntent().getStringExtra("title");
+        if (!TextUtils.isEmpty(title)) {
+            if (title.length() > 8) {
+                titleView.setTopText(title.substring(0, 6)+"...");
+            } else {
+                titleView.setTopText(title);
+            }
         }
     }
 
     @Override
     protected void doMain() {
-        String url=getIntent().getStringExtra("url");
+        String url = getIntent().getStringExtra("url");
         mAgentWeb = AgentWeb.with(this)//传入Activity
-        .setAgentWebParent(ll_web, new LinearLayout.LayoutParams(-1, -1))//传入AgentWeb 的父控件 ，如果父控件为 RelativeLayout ， 那么第二参数需要传入 RelativeLayout.LayoutParams
-        .useDefaultIndicator()// 使用默认进度条
-        .defaultProgressBarColor() // 使用默认进度条颜色
-        .createAgentWeb()//
-        .ready()
-        .go(url);
+                .setAgentWebParent(ll_web, new LinearLayout.LayoutParams(-1, -1))//传入AgentWeb 的父控件 ，如果父控件为 RelativeLayout ， 那么第二参数需要传入 RelativeLayout.LayoutParams
+                .useDefaultIndicator()// 使用默认进度条
+                .defaultProgressBarColor() // 使用默认进度条颜色
+                .createAgentWeb()//
+                .ready()
+                .go(url);
     }
 }
