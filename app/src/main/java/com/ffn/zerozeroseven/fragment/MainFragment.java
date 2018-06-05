7537b6fde54265e0616e0f448da72b2536357c28
mrsun <mrsun1127@163.com>
2018-06-05 11:50:26 +0800

    fristCommit

--- app/src/main/java/com/ffn/zerozeroseven/fragment/MainFragment.java
+++ app/src/main/java/com/ffn/zerozeroseven/fragment/MainFragment.java
@@ -1301,16 +1301,15 @@ public class MainFragment extends BaseFragment implements View.OnClickListener, 
                 }
                 break;
             case R.id.rl_integer:
-                ToastUtils.showShort("该功能暂未开放");
-//                if (userInfo != null) {
-//                    if ("943478288".equals(schoolIId)) {
-//                        ToastUtils.showShort("请先选择学校");
-//                    } else {
-//                        ZeroZeroSevenUtils.SwitchActivity(bfCxt, IntegralDrawActivity.class);
-//                    }
-//                } else {
-//                    ZeroZeroSevenUtils.SwitchActivity(bfCxt, LoginActivity.class);
-//                }
+                if (userInfo != null) {
+                    if ("943478288".equals(schoolIId)) {
+                        ToastUtils.showShort("请先选择学校");
+                    } else {
+                        ZeroZeroSevenUtils.SwitchActivity(bfCxt, IntegralDrawActivity.class);
+                    }
+                } else {
+                    ZeroZeroSevenUtils.SwitchActivity(bfCxt, LoginActivity.class);
+                }
                 break;
             case R.id.rl_local:
                 if (userInfo != null) {
