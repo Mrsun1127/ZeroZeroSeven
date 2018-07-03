c75422b74149d87616ceb5efd8400cb808924921
mrsun <mrsun1127@163.com>
2018-07-03 17:41:02 +0800

    fristCommit

---
+++ app/src/main/java/com/ffn/zerozeroseven/ui/NumberRicalDetilsActivity.java
@@ -0,0 +1,16 @@
+package com.ffn.zerozeroseven.ui;
+
+import com.ffn.zerozeroseven.R;
+import com.ffn.zerozeroseven.base.BaseActivity;
+
+public class NumberRicalDetilsActivity extends BaseActivity {
+    @Override
+    protected int setLayout() {
+        return R.layout.activity_numberical_detils;
+    }
+
+    @Override
+    protected void doMain() {
+
+    }
+}
