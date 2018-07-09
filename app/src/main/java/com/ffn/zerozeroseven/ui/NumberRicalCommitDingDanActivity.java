9db450196e5516356dafe862cad048cdc94a4498
mrsun <mrsun1127@163.com>
2018-07-09 12:07:00 +0800

    delete

---
+++ app/src/main/java/com/ffn/zerozeroseven/ui/NumberRicalCommitDingDanActivity.java
@@ -0,0 +1,16 @@
+package com.ffn.zerozeroseven.ui;
+
+import com.ffn.zerozeroseven.R;
+import com.ffn.zerozeroseven.base.BaseActivity;
+
+public class NumberRicalCommitDingDanActivity extends BaseActivity {
+    @Override
+    protected int setLayout() {
+        return R.layout.activity_numberical_commitdingdan;
+    }
+
+    @Override
+    protected void doMain() {
+
+    }
+}
