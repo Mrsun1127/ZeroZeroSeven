1f1ac472693f06d48bbbfaa740068011df6254b8
mrsun <mrsun1127@163.com>
2018-07-09 16:02:18 +0800

    delete

---
+++ app/src/main/java/com/ffn/zerozeroseven/ui/NumberDrawBackActivity.java
@@ -0,0 +1,16 @@
+package com.ffn.zerozeroseven.ui;
+
+import com.ffn.zerozeroseven.R;
+import com.ffn.zerozeroseven.base.BaseActivity;
+
+public class NumberDrawBackActivity extends BaseActivity {
+    @Override
+    protected int setLayout() {
+        return R.layout.activity_drawback;
+    }
+
+    @Override
+    protected void doMain() {
+
+    }
+}
