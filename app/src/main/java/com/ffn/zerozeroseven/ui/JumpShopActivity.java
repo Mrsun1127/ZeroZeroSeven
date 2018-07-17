6915e2beaf593ee255b86825e9b2d122b6dce88b
mrsun <mrsun1127@163.com>
2018-07-17 17:59:11 +0800

    update

--- app/src/main/java/com/ffn/zerozeroseven/ui/JumpShopActivity.java
+++ app/src/main/java/com/ffn/zerozeroseven/ui/JumpShopActivity.java
@@ -1,13 +1,45 @@
 package com.ffn.zerozeroseven.ui;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.widget.LinearLayout;
+
 import com.ffn.zerozeroseven.R;
+import com.ffn.zerozeroseven.adapter.ExamplePagerAdapter;
 import com.ffn.zerozeroseven.base.BaseActivity;
+import com.ffn.zerozeroseven.utlis.ToastUtils;
 
 
+import net.lucode.hackware.magicindicator.MagicIndicator;
+import net.lucode.hackware.magicindicator.ViewPagerHelper;
+import net.lucode.hackware.magicindicator.buildins.UIUtil;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.CommonNavigator;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.CommonNavigatorAdapter;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.IPagerIndicator;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.IPagerTitleView;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.indicators.LinePagerIndicator;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.titles.ColorTransitionPagerTitleView;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.titles.SimplePagerTitleView;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.titles.badge.BadgePagerTitleView;
+
+import java.util.Arrays;
+import java.util.List;
+
+import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class JumpShopActivity extends BaseActivity {
-
+    @Bind(R.id.magic_indicator4)
+    MagicIndicator magic_indicator4;
+    @Bind(R.id.viewpage)
+    ViewPager viewpage;
+    private static final String[] CHANNELS = new String[]{"看最新", "逛热门"};
+    private List<String> mDataList = Arrays.asList(CHANNELS);
+    private ExamplePagerAdapter mExamplePagerAdapter = new ExamplePagerAdapter(mDataList);
 
     @Override
     protected int setLayout() {
@@ -21,6 +53,66 @@ public class JumpShopActivity extends BaseActivity {
 
     @Override
     protected void doMain() {
+        viewpage.setAdapter(mExamplePagerAdapter);
+        initMagicIndicator4();
+    }
 
+    private void initMagicIndicator4() {
+        CommonNavigator commonNavigator = new CommonNavigator(this);
+        commonNavigator.setAdapter(new CommonNavigatorAdapter() {
+
+            @Override
+            public int getCount() {
+                return mDataList.size();
+            }
+
+            @Override
+            public IPagerTitleView getTitleView(Context context, final int index) {
+                BadgePagerTitleView badgePagerTitleView = new BadgePagerTitleView(context);
+
+                SimplePagerTitleView simplePagerTitleView = new ColorTransitionPagerTitleView(context);
+                simplePagerTitleView.setNormalColor(Color.BLACK);
+                simplePagerTitleView.setSelectedColor(getResources().getColor(R.color.money));
+                simplePagerTitleView.setText(mDataList.get(index));
+                simplePagerTitleView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        viewpage.setCurrentItem(index);
+                    }
+                });
+                badgePagerTitleView.setInnerPagerTitleView(simplePagerTitleView);
+
+                return badgePagerTitleView;
+            }
+
+            @Override
+            public IPagerIndicator getIndicator(Context context) {
+                LinePagerIndicator linePagerIndicator = new LinePagerIndicator(context);
+                linePagerIndicator.setColors(getResources().getColor(R.color.money));
+                return linePagerIndicator;
+            }
+        });
+        magic_indicator4.setNavigator(commonNavigator);
+        LinearLayout titleContainer = commonNavigator.getTitleContainer(); // must after setNavigator
+        titleContainer.setShowDividers(LinearLayout.SHOW_DIVIDER_MIDDLE);
+        titleContainer.setDividerDrawable(new ColorDrawable() {
+            @Override
+            public int getIntrinsicWidth() {
+                return UIUtil.dip2px(JumpShopActivity.this, 15);
+            }
+        });
+        ViewPagerHelper.bind(magic_indicator4, viewpage);
+    }
+
+    @OnClick({R.id.rl_back, R.id.rl_post})
+    void setOnClicks(View v) {
+        switch (v.getId()) {
+            case R.id.rl_back:
+                finish();
+                break;
+            case R.id.rl_post:
+                ToastUtils.showShort("发表");
+                break;
+        }
     }
 }
