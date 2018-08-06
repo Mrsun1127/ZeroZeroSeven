3491fc234cca03203d5c128bafe181bf0e2bc9a5
mrsun <mrsun1127@163.com>
2018-08-06 12:18:26 +0800

    add

---
+++ app/src/main/java/com/ffn/zerozeroseven/ui/ErrandDingDanDetilsActivity.java
@@ -0,0 +1,16 @@
+package com.ffn.zerozeroseven.ui;
+
+import com.ffn.zerozeroseven.R;
+import com.ffn.zerozeroseven.base.BaseActivity;
+
+public class ErrandDingDanDetilsActivity extends BaseActivity {
+    @Override
+    protected int setLayout() {
+        return R.layout.activity_errand_detils;
+    }
+
+    @Override
+    protected void doMain() {
+
+    }
+}
